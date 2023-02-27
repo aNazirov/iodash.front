869f4d34010cbb2f1cc86dec83715640e7d8c604
aNazirov_ <nazirov.bobur01@mail.ru>
2023-02-27 11:33:16 +0500

    fix: -----

--- core/api/index.ts
+++ core/api/index.ts
@@ -4,16 +4,16 @@ import { Toast } from "../helpers/utils";
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    // origin: process.env.NEXT_PUBLIC_HOST,
-    origin: undefined,
+    origin: process.env.NEXT_PUBLIC_HOST,
+    // origin: undefined,
   },
 });
 
 const fileInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_FILE_API_URL,
   headers: {
-    // origin: process.env.NEXT_PUBLIC_HOST,
-    origin: undefined,
+    origin: process.env.NEXT_PUBLIC_HOST,
+    // origin: undefined,
   },
 });
 
